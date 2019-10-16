fdf7023cb9d1a3c0d8e0afbc18c034079d20296b
Jiayuan Zhang <jzhang@well.ox.ac.uk>
2019-10-16 21:36:01 +0100

    Add whole_genome_flye

---
+++ analysis/whole_genome_flye/scripts/flye.sge.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Specify a job name
+#$ -N Whole_gemome_flye
+
+# Project name and target queue
+#$ -P todd.prjc
+#$ -q himem.qh
+
+# Run the job in the current working directory
+#$ -cwd -j y
+
+# Log locations which are relative to the current
+# working directory of the submission
+#$ -o qsub_output.log
+#$ -e qsub_error.log
+
+# Parallel environemnt settings
+#  For more information on these please see the wiki
+#  Allowed settings:
+#   shmem
+#   mpi
+#   node_mpi
+#   ramdisk
+#$ -pe shmem 10
+
+# Some useful data about the job to help with debugging
+echo "------------------------------------------------"
+echo "SGE Job ID: $JOB_ID"
+echo "SGE Task ID: $SGE_TASK_ID"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+# Begin writing your script here
+
+FASTQ=/well/longread/projects/nanopore/data/WTON000393/fastq-trimmed-all.fastq.gz
+OUTPUT_FOLDER=Flye
+/well/ont/apps/Flye-2.5/bin/flye --nano-raw $FASTQ -g 3.2g -o $OUTPUT_FOLDER -t 4 --asm-coverage 40
+echo "Assembly complete"
+
+# End of job script
